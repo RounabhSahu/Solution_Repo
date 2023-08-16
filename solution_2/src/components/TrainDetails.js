0122b0eada5ecd6771a8cb146fdc9656d1e8a8e8
Rounabh Sahu <godemode12@gmail.com>
2023-08-16 23:06:42 +0530

    Added Output And Improved Styles

--- solution_2/src/components/TrainDetails.js
+++ solution_2/src/components/TrainDetails.js
@@ -1,14 +1,51 @@
-// components/TrainDetails.js
 import React from 'react';
 
 const TrainDetails = ({ train }) => {
   return (
-    <div>
+    <div className="bg-white rounded-lg shadow-md p-6">
       <h1 className="text-2xl font-bold mb-4">Train Details</h1>
-      <p>Name: {train.trainName}</p>
-      <p>Number: {train.trainNumber}</p>
-      <p>Departure Time: {train.departureTime.Hours}:{train.departureTime.Minutes}</p>
-
+      <div className="grid grid-cols-2 gap-4">
+        <div>
+          <p className="text-gray-600">Name:</p>
+          <p className="text-xl">{train.trainName}</p>
+        </div>
+        <div>
+          <p className="text-gray-600">Number:</p>
+          <p className="text-xl">{train.trainNumber}</p>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-4 mt-4">
+        <div>
+          <p className="text-gray-600">Departure Time:</p>
+          <p className="text-xl">
+            {train.departureTime.Hours}:{train.departureTime.Minutes}
+          </p>
+        </div>
+        <div>
+          <p className="text-gray-600">Seats Available (Sleeper):</p>
+          <p className="text-xl">{train.seatsAvailable.sleeper}</p>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-4 mt-4">
+        <div>
+          <p className="text-gray-600">Seats Available (AC):</p>
+          <p className="text-xl">{train.seatsAvailable.AC}</p>
+        </div>
+        <div>
+          <p className="text-gray-600">Price (Sleeper):</p>
+          <p className="text-xl">{train.price.sleeper}</p>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-4 mt-4">
+        <div>
+          <p className="text-gray-600">Price (AC):</p>
+          <p className="text-xl">{train.price.AC}</p>
+        </div>
+        <div>
+          <p className="text-gray-600">Delayed By (min):</p>
+          <p className="text-xl">{train.delayedBy}</p>
+        </div>
+      </div>
     </div>
   );
 };
--- solution_2/src/components/TrainsList.js
+++ solution_2/src/components/TrainsList.js
@@ -1,20 +1,55 @@
-// components/TrainList.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const TrainsList = ({ trains }) => {
   return (
-    <div>
+    <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Train List</h1>
-      <ul>
-        {trains.map(train => (
-          <li key={train.trainNumber} className="mb-2">
-            <Link to={`/train/${train.trainNumber}`} className="text-blue-500 underline">
-              {train.trainName} ({train.trainNumber})
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <div className="overflow-x-auto">
+        <table className="w-full border-collapse">
+          <thead>
+            <tr className="bg-gray-200">
+              <th className="border p-2">Train Name</th>
+              <th className="border p-2">Train Number</th>
+              <th className="border p-2">Departure Time</th>
+              <th className="border p-2">Seats Available (Sleeper)</th>
+              <th className="border p-2">Seats Available (AC)</th>
+              <th className="border p-2">Price (Sleeper)</th>
+              <th className="border p-2">Price (AC)</th>
+              <th className="border p-2">Delayed By (min)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {trains.map(train => (
+              <tr
+                key={train.trainNumber}
+                className="cursor-pointer hover:bg-gray-100"
+              >
+                <td className="border p-2">
+                  <Link
+                    to={`/train/${train.trainNumber}`}
+                    className="text-blue-500 underline"
+                  >
+                    {train.trainName}
+                  </Link>
+                </td>
+                <td className="border p-2">{train.trainNumber}</td>
+                <td className="border p-2">
+                  {train.departureTime.Hours}:
+                  {train.departureTime.Minutes < 10
+                    ? '0' + train.departureTime.Minutes
+                    : train.departureTime.Minutes}
+                </td>
+                <td className="border p-2">{train.seatsAvailable.sleeper}</td>
+                <td className="border p-2">{train.seatsAvailable.AC}</td>
+                <td className="border p-2">{train.price.sleeper}</td>
+                <td className="border p-2">{train.price.AC}</td>
+                <td className="border p-2">{train.delayedBy}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
